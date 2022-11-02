fbdd13429a0378387bc796ea61bf8b75ecb0725e
polskin <mark.pollard6@btinternet.com>
2022-11-02 11:10:12 +0000

    updated database

---
+++ TraineeTracker/TraineeTrackerApp/Migrations/20221102110456_Initial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TraineeTrackerApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CourseId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_CourseId",
+                table: "AspNetUsers",
+                column: "CourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Courses_CourseId",
+                table: "AspNetUsers",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Courses_CourseId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_CourseId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CourseId",
+                table: "AspNetUsers");
+        }
+    }
+}
